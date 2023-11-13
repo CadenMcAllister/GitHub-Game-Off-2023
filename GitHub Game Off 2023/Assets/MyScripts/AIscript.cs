e031b374b7331fdbdb542aedf867bfb398f6a78f
CadenMcAllister <126972887+CadenMcAllister@users.noreply.github.com>
2023-11-12 20:05:01 -0600

    Random Changes

--- GitHub Game Off 2023/Assets/MyScripts/AIscript.cs
+++ GitHub Game Off 2023/Assets/MyScripts/AIscript.cs
@@ -55,7 +55,7 @@ public class AIscript : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.tag == "Projectile"){
+        if (collision.gameObject.tag == "Player"){
         collisionCounter += 1;
         }
     }
